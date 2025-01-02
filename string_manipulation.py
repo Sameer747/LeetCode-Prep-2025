831a4edc5334c21ebddc8d93c1a0a83ce286abe5
Sameer747 <sameer.mohsin@live.com>
2025-01-03 01:13:09 +0500

    string manipultaion

---
+++ string_manipulation.py
@@ -0,0 +1,14 @@
+course = "Pyython is great"
+indices = []
+index = 0
+
+while True:
+    index = course.find('Pyython', index)  # Find 'Pyython' starting from the current position
+    if index == -1:
+        break  # Stop if 'Pyython' is not found
+    # Append the index of each character in the word 'Pyython'
+    for i in range(len('Pyython')):
+        indices.append(index + i)
+    index += 1  # Move to the next character for the next search
+
+print(indices)
