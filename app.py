c06bc10c9cb81bb158e4291a1f7b5cbfbb0ca432
Sameer747 <sameer.mohsin@live.com>
2025-01-03 00:48:25 +0500

    learned about variables, user input,type casting

--- app.py
+++ app.py
@@ -1,2 +1,20 @@
 # first pyhton code
 print('Hello World')
+
+# variables: Use to temporarily store data into computer
+age  = 20 #int
+price = 19.95 #double
+firstName = 'Sameer' #string
+isOnline = False #boolean
+print(firstName + " " + str(age) + " " + str(price) + " " + str(isOnline))
+
+# user input with type casting 
+# by default python takes input as strings and later it is converted
+name = str(input("Name: "))
+age = str(float(input('Age: ')))
+print(name + " \n" +age)
+# Exercise
+firstNo = float(input("First: "))
+secondNo = float(input("Second: "))
+sum = str(firstNo + secondNo)
+print('total sum is: '+ sum)
